7e9516b6f500947704692c321faeb7f81839db33
Lil trash can <44183898+oscarwooberry@users.noreply.github.com>
2025-06-17 17:37:35 -0400

    modify install script

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="guided-diffusion",
-    py_modules=["guided_diffusion"],
+    packages=["guided_diffusion"],
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
 )
